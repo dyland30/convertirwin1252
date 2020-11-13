a01add24d63b2f962b795fd226fe004c613283f4
Castillo <dcastillo@lima-airport.com>
2020-11-12 20:26:50 -0500

    primer commit

---
+++ cambiar_codificacion_txt/Codificador.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cambiar_codificacion_txt
+{
+    public class Codificador
+    {
+        public void ConvertirListaWindows1252(string[] archivos)
+        {
+
+            foreach (string archivo in archivos)
+            {
+                ConvertirArchivoWindows1252(archivo);
+                Console.WriteLine($"Se cambió la codificación del archivo {archivo}");
+
+            }
+        }
+
+        public void ConvertirArchivoWindows1252(string archivo)
+        {
+            //leer archivo
+            String[] lineas = File.ReadAllLines(archivo);
+            //crear archivo
+            //eliminar archivo anterior
+            File.Delete(archivo);
+            using (TextWriter tw = new StreamWriter(archivo, true, Encoding.GetEncoding(1252)))
+            {
+                foreach (String linea in lineas)
+                {
+                    tw.WriteLine(linea);
+                }
+            }
+        }
+
+    }
+}
---
+++ cambiar_codificacion_txt/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cambiar_codificacion_txt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Codificador codificador = new Codificador();
+                if (args.Length < 2 || args.Length > 4)
+                {
+                    string ayuda = @"
+                Opciones disponibles:
+                cambiar_codificacion_txt.exe -f <ruta_archivo>
+                cambiar_codificacion_txt.exe -r <ruta_carpeta> (por defecto extensión .txt)
+                cambiar_codificacion_txt.exe -r <ruta_carpeta> -e <extension>
+                Ejemplo: cambiar_codificacion_txt.exe -r D:\carpeta -e csv";
+                    Console.Write(ayuda);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    if (args[0].ToLower() == "-r" && args.Length == 2)
+                    {
+                        //extensión txt por defecto
+                        string rutaCarpeta = args[1];
+                        string[] fileArray = Directory.GetFiles(rutaCarpeta, "*.txt");
+                        if (fileArray != null && fileArray.Length > 0)
+                        {
+                            codificador.ConvertirListaWindows1252(fileArray);
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se encontró ningún archivo con la extensión txt");
+                        }
+
+                        
+
+                    }
+                    else if (args[0].ToLower() == "-r" && args.Length == 4)
+                    {
+                        //extension personalizada
+                        string rutaCarpeta = args[1];
+                        string ext = args[3].ToLower();
+
+                        string[] fileArray = Directory.GetFiles(rutaCarpeta, $"*.{ext}");
+
+                        if (fileArray != null && fileArray.Length > 0)
+                        {
+                            codificador.ConvertirListaWindows1252(fileArray);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"No se encontró ningún archivo con la extensión {ext}");
+                        }
+
+                       
+
+                    }
+
+                    else if (args[0].ToLower() == "-f")
+                    {
+
+                        string archivo = args[1];
+                        codificador.ConvertirArchivoWindows1252(archivo);
+                        Console.WriteLine($"Se cambió la codificación del archivo {archivo}");
+                        
+
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+
+
+
+            }
+
+           
+
+        }
+    }
+}
